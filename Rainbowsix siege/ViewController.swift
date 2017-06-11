bca3b8c53f1c734ba4dcaf85b2107b736a9a9c24
江口太郎 <kisyuwado2199@gmail.com>
2017-06-11 16:29:16 +0900

    inital commit

---
+++ Rainbowsix siege/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Rainbowsix siege
+//
+//  Created by 江口太郎 on 2017/05/28.
+//  Copyright © 2017年 None. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
